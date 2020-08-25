a37168f0fe454870e85043a13aea7f5716991f2d
samg11 <Sam.Girshovich@gmail.com>
2020-08-25 15:17:42 -0400

    basic frontend

---
+++ script/script.js
@@ -0,0 +1,14 @@
+function rock() {
+	var userChoice = "rock";
+	console.log(userChoice)
+}
+
+function paper() {
+	var userChoice = "paper";
+	console.log(userChoice)
+}
+
+function scissors() {
+	var userChoice = "scissors";
+	console.log(userChoice)
+}
